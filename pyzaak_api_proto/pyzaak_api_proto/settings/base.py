248d28654b7c9661df67bf8fdb2e7da834014fa5
Zona <bryson.gilreath@gmail.com>
2018-09-23 15:51:22 -0500

    fixed path setting

--- pyzaak_api_proto/pyzaak_api_proto/settings/base.py
+++ pyzaak_api_proto/pyzaak_api_proto/settings/base.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+DJANGO_PROJECT_DIR = os.path.dirname(BASE_DIR)
+PROJECT_DIR = os.path.dirname(DJANGO_PROJECT_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
