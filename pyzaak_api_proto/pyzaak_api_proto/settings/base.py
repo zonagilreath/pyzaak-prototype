2196afc644fd692c562ba935cc88b532e0f61cdd
Zona <bryson.gilreath@gmail.com>
2018-09-26 22:37:50 -0500

    fixing static files

--- pyzaak_api_proto/pyzaak_api_proto/settings/base.py
+++ pyzaak_api_proto/pyzaak_api_proto/settings/base.py
@@ -123,8 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "static"),
+]
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
