304275b603281054eace8b718d50f96adb8bf2fe
Zona <bryson.gilreath@gmail.com>
2018-09-26 22:10:15 -0500

    added frontend

---
+++ pyzaak_api_proto/frontend/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/profile/', views.home, name='profile')
+]
